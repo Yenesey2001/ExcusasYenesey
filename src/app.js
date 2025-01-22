5d67ec33214172732139d11c0587c692d8dee19a
Yenesey2001 <yeneseyclase@gmail.com>
2025-01-22 20:16:14 +0100

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// Función para generar excusas
+window.onload = function () {
+  generarExcusa();
 };
+
+function generarExcusa() {
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let rnd1 = Math.floor(Math.random() * who.length);
+  let rnd2 = Math.floor(Math.random() * action.length);
+  let rnd3 = Math.floor(Math.random() * what.length);
+  let rnd4 = Math.floor(Math.random() * when.length);
+
+  // Genera la excusa combinando las partes
+  let excuse = `${who[rnd1]} ${action[rnd2]} ${what[rnd3]} ${when[rnd4]}`;
+
+  // Muestra la excusa en el elemento con id "excuse"
+  document.querySelector("#excuse").innerHTML = excuse;
+}
